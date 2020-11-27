5e06dc8de696dc3a64fe267e6b008ea09f981f18
NickPuchko <47064996+NickPuchko@users.noreply.github.com>
2020-11-27 16:39:26 +0300

    init

---
+++ iOS_ScienceCore/iOS_ScienceCore/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  iOS_ScienceCore
+//
+//  Created by Administrator on 27.11.2020.
+//
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    }
+
+
+
+}
